ef5ee5dbb8539e06f2aa58322420c40c4893843f
kdund <knut.mora@fysik.su.se>
2016-04-08 17:42:35 +0200

    Add kernel task to SVM slide

--- Scripts/kernel_density.py
+++ Scripts/kernel_density.py
@@ -7,7 +7,16 @@ training_data0 = np.load('ClassSample_training_0.npy')
 training_data1 = np.load('ClassSample_training_1.npy')
 training_data_full = np.concatenate((training_data0, training_data1))
 
+
 training_data_full = training_data_full[~np.isnan(training_data_full).any(axis=1)]
+training_data0 = training_data0[~np.isnan(training_data0).any(axis=1)]
+training_data1 = training_data1[~np.isnan(training_data1).any(axis=1)]
+
+
+pg = (1.*len(training_data1))/(1.*len(training_data_full))
+pp = (1.*len(training_data0))/(1.*len(training_data_full))
+
+
 np.random.shuffle(training_data_full)
 
 n_samples = training_data_full.shape[0]
@@ -21,7 +30,7 @@ labels = training_set[:, -1]
 pl.figure()
 ax1 = pl.subplot(121)
 ax2 = pl.subplot(122)
-for n_clusters in [2, 5, 10, 15]:
+for n_clusters in [2]:
     print n_clusters
     gmmb = GMMBayes(n_clusters)
     gmmb.fit(features, labels)
@@ -44,10 +53,27 @@ ax1.set_ylabel('True positive rate')
 ax1.legend(loc='best')
 
 # Predict on test set
-predictions = gmmb.predict(test_set[:, :-1])
+predictions = gmmb.predict_proba(test_set[:, :-1])
+print predictions.shape
+predictions = predictions[:,0]
 true_labels = test_set[:, -1]
-missclassifications = np.sum(np.abs(predictions-true_labels))
-correct_frac = 1. - missclassifications/float(len(true_labels))
-print 'Correct fraction: ', correct_frac
 
-pl.show()
+#Find 50% point for background: 
+print "pp"  ,pp
+cut50 = np.percentile(predictions[true_labels==0],100.*(1.-0.5/pp))
+
+print cut50
+
+print "false positive probability given 0"
+#print cut50<predictions[true_labels==0.]
+print (1.*(cut50<predictions[true_labels==0.]).sum())/(1.*len(predictions[true_labels==0]))
+print "false positive probability for all events"
+#print cut50<predictions[true_labels==0.]
+print (1.*(cut50<predictions[true_labels==0.]).sum())/(1.*len(predictions))
+
+#print predictions
+
+#testfpt= 1.*(predictions==true_labels and true_labels==1).sum()/(1.*len(predictions))
+print "true positive fraction:" 
+
+#pl.show()
